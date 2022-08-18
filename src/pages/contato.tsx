6ff2369695ebf786d9071a01f09f2ab378cdc3a4
Mariana Luiza Vitor Alves Silva <mariana.luiza@spcbrasil.org.br>
2022-08-18 10:02:25 -0300

    rename pages

---
+++ src/pages/contato.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import style from './../styles/index.module.scss'
+
+export default function Contato() {
+  return (
+    <div className={style.Home}>
+      <p>Este é o conteúdo da Contato</p>
+    </div>
+  )
+}
