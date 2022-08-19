4c7c296cc5e2c7f08719c68bfa6f0a323bbb9cd8
Mariana Luiza Vitor Alves Silva <mariana.luiza@spcbrasil.org.br>
2022-08-19 10:52:13 -0300

    melhorando estilo da apresentacao

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f7466594288c2b82"; }
+/******/ 		__webpack_require__.h = function() { return "df5fdbfdeb9bbca9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
